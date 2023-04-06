9d4a3f46cab2a5e5e39251d535a254b34c938531
Okutubo Lekan Michael <86299143+olakved@users.noreply.github.com>
2023-04-06 15:09:32 +0100

    correction

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -37,7 +37,7 @@ function AppLayout() {
           } duration-300 h-screen relative p-5 pt-8 bg-dark-green `}
         >
           <p
-            className="absolute cursor-pointer rounded-full -right-3 top-9 w-7 h-7 text-center border-2 border-dark-green bg-white sm:hidden"
+            className="absolute cursor-pointer rounded-full -right-3 top-9 w-7 h-7 text-center border-2 border-dark-green bg-white"
             onClick={() => setOpen(!open)}
           >
             +
