b9b07c4f4249879af67f2b53e9c21c91b0de2cc7
Okutubo Lekan Michael <86299143+olakved@users.noreply.github.com>
2023-04-08 02:26:26 +0100

    header: correction

--- src/components/headerN/index.jsx
+++ src/components/headerN/index.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/logo.png";
 import { Link } from "react-router-dom";
+import menuIcon from "../../assets/menuIcon.png";
 
 function HeaderN() {
   const links = [
@@ -10,25 +11,41 @@ function HeaderN() {
     { name: "ABOUT", link: "/about" },
     { name: "CONTACT", link: "/contact" },
   ];
+
+  const [open, setOpen] = useState(false);
   return (
     <div className="shadow-md w-full fixed top-0 left-0">
-      <div className="flex justify-between items-center bg-white py-4 md:px-10 px-7">
+      <div className="flex justify-between items-center bg-white py-4 md:px-10 px-7 md:flex-col">
         <div className="font-bold text-2xl cursor-pointer flex items-center text-dark-green">
-          <span className="mr-1 pt-2">
+          <span className="mr-3 ml-8">
             <img src={logo} alt="logo" className="rounded-full w-10 h-10" />
           </span>
           <span>Cocsho</span>
         </div>
-        <ul className="flex gap-5 md:items-center">
-          {links.map((link) => (
-            <li
-              key={link.name}
-              className="md:ml-8 text-xl hover:text-gray-400 duration-500"
-            >
-              <Link to={link.link}>{link.name}</Link>
-            </li>
-          ))}
-        </ul>
+        <div className={`${!open ? "md:hidden" : ""}`}>
+          <ul
+            className="flex gap-5 md:items-center md:flex-col md:pb-0 md:mt-5 md:static bg-white  md:w-auto  transition-all duration-500 ease-in ${
+            "
+          >
+            {links.map((link) => (
+              <li
+                key={link.name}
+                className="md:ml-8 text-l hover:text-gray-400 duration-500 md:my-0 my-2"
+              >
+                <Link to={link.link}>{link.name}</Link>
+              </li>
+            ))}
+            <button className="bg-dark-green text-xl text-white py-[3px] px-6 rounded-md ml-6 hover:bg-light-green duration-500">
+              Login
+            </button>
+          </ul>
+        </div>
+        <div
+          className="absolute right-8 top-8 cursor-pointer invisible md:visible "
+          onClick={() => setOpen(!open)}
+        >
+          <img src={menuIcon} alt="" className="w-5" />
+        </div>
       </div>
     </div>
   );
